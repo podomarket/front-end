a7f61b35ecb6f6477a1e4254a20c6e0a38e7c550
ose1012 <nnmmaz95@gmail.com>
2022-10-21 16:14:28 +0900

    깃 머지 테스트

--- podomaket/src/App.js
+++ podomaket/src/App.js
@@ -1,8 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">test</div>;
+  return <div className="App">오성은 깃 머지 테스트입니다!</div>;
 }
 
 export default App;

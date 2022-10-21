edc184af8c024f18fe6ea837d167698e523ac554
soyoung <0001401@naver.com>
2022-10-21 16:06:22 +0900

    test3

--- podomaket/src/App.js
+++ podomaket/src/App.js
@@ -1,8 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">test1</div>;
+  return <div className="App">test</div>;
 }
 
 export default App;

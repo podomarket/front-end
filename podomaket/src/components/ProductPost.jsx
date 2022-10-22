2024d0572ba16b60e803f9b728c6be7cecf37dc9
ose1012 <nnmmaz95@gmail.com>
2022-10-23 01:56:29 +0900

    시간 테스트2

--- podomaket/src/components/ProductPost.jsx
+++ podomaket/src/components/ProductPost.jsx
@@ -12,35 +12,16 @@ import {
   UploadName,
   Wrap,
 } from "../style/productPost_styled";
-// 안써도 자동으로 한국 시간을 불러온다. 명확하게 하기 위해 import
 
 const ProductPost = () => {
   const dispatch = useDispatch();
 
-  const detailDate = (a) => {
-    const milliSeconds = new Date() - a;
-    const seconds = milliSeconds / 1000;
-    if (seconds < 60) return `방금 전`;
-    const minutes = seconds / 60;
-    if (minutes < 60) return `${Math.floor(minutes)}분 전`;
-    const hours = minutes / 60;
-    if (hours < 24) return `${Math.floor(hours)}시간 전`;
-    const days = hours / 24;
-    if (days < 7) return `${Math.floor(days)}일 전`;
-    const weeks = days / 7;
-    if (weeks < 5) return `${Math.floor(weeks)}주 전`;
-    const months = days / 30;
-    if (months < 12) return `${Math.floor(months)}개월 전`;
-    const years = days / 365;
-    return `${Math.floor(years)}년 전`;
-  };
-  const nowDate = detailDate(new Date());
   const [products, setProducts] = useState({
     title: "",
     imageUrl: "",
     content: "",
     price: 0,
-    date: nowDate,
+    date: new Date().getTime(),
   });
 
   const onChangeHandler = (e) => {
@@ -59,7 +40,7 @@ const ProductPost = () => {
       imageUrl: "",
       content: "",
       price: 0,
-      date: nowDate,
+      date: new Date().getTime(),
     });
   };
 
